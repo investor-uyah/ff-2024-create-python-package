8f41276488ea718c4cd1f73c9a44e1fe9e111d37
Mfonobong Uyah <97167651+investor-uyah@users.noreply.github.com>
2024-10-30 16:15:26 +0000

    add: module package

---
+++ src/m_ffirst_package/add_number.py
@@ -0,0 +1,19 @@
+"""A module that adds numbers together"""
+
+def add_num(num1, num2):
+    """A function that adds two numbers together
+    
+    Parameters
+    ----------
+
+    num1 : int
+        The first number that the function will add.
+    num2 : int 
+        The second number that the function will add.
+    
+    Return
+    ------
+        The sum of two numbers
+        
+    """
+    return num1 + num2
